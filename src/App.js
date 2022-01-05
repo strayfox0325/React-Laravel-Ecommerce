087b3ddd8772eebb39c18d1bf97e16d3bec507a9
Isidora Lazic <isidora@ishidoranoMacBook-Pro.local>
2022-01-05 23:12:20 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {BrowserRouter as Router,Redirect,Route,Switch} from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import MasterLayout from './layouts/admin/MasterLayout';
+import Home from './components/front/Home';
+import Login from './components/front/Auth/Login';
+import Register from './components/front/Auth/Register';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route path="/register" component={Register}/>
+          <Route path="/login" component={Login} />
+          <Route path="/admin" name="Admin" render={(props)=><MasterLayout {...props} /> }/>
+        </Switch>
+      </Router>
     </div>
   );
 }
